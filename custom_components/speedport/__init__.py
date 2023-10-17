b5d7bba9f5669315a2b79af1f6323b5417e37ad0
Andre Basche <andre.basche@gmail.com>
2023-10-17 23:47:05 +0200

    Configure login pause #2

--- custom_components/speedport/__init__.py
+++ custom_components/speedport/__init__.py
@@ -1,11 +1,11 @@
 """The Speedport integration."""
-
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import aiohttp_client
 from speedport import Speedport
 
+from .config_flow import OptionsFlowHandler
 from .const import DOMAIN
 
 PLATFORMS: list[Platform] = [
@@ -19,20 +19,30 @@ PLATFORMS: list[Platform] = [
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up Speedport from a config entry."""
-    session = aiohttp_client.async_get_clientsession(hass)
 
     hass.data.setdefault(DOMAIN, {})
     speedport = await Speedport(
-        host=entry.data["host"], password=entry.data["password"], session=session
+        host=entry.data["host"],
+        password=entry.data["password"],
+        session=aiohttp_client.async_get_clientsession(hass),
+        pause_time=entry.options.get("pause_time", 5),
     ).create()
     hass.data[DOMAIN][entry.entry_id] = speedport
     hass.data[DOMAIN]["coordinators"] = {}
+
+    entry.async_on_unload(entry.add_update_listener(update_listener))
 
     for platform in PLATFORMS:
         hass.async_create_task(
             hass.config_entries.async_forward_entry_setup(entry, platform)
         )
     return True
+
+
+async def update_listener(hass, entry):
+    """Handle options update."""
+    speedport: Speedport = hass.data[DOMAIN][entry.entry_id]
+    speedport.set_pause_time(entry.options.get("pause_time", 5))
 
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
--- custom_components/speedport/config_flow.py
+++ custom_components/speedport/config_flow.py
@@ -8,7 +8,7 @@ from speedport import Speedport
 import voluptuous as vol
 
 from homeassistant import config_entries
-from homeassistant.core import HomeAssistant
+from homeassistant.core import HomeAssistant, callback
 from homeassistant.data_entry_flow import FlowResult
 from homeassistant.exceptions import HomeAssistantError
 
@@ -76,6 +76,14 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
             step_id="user", data_schema=STEP_USER_DATA_SCHEMA, errors=errors
         )
 
+    @staticmethod
+    @callback
+    def async_get_options_flow(
+        config_entry: config_entries.ConfigEntry,
+    ) -> config_entries.OptionsFlow:
+        """Create the options flow."""
+        return OptionsFlowHandler(config_entry)
+
 
 class CannotConnect(HomeAssistantError):
     """Error to indicate we cannot connect."""
@@ -83,3 +91,28 @@ class CannotConnect(HomeAssistantError):
 
 class InvalidAuth(HomeAssistantError):
     """Error to indicate there is invalid auth."""
+
+
+class OptionsFlowHandler(config_entries.OptionsFlow):
+    def __init__(self, config_entry: config_entries.ConfigEntry) -> None:
+        """Initialize options flow."""
+        self.config_entry = config_entry
+
+    async def async_step_init(
+        self, user_input: dict[str, Any] | None = None
+    ) -> FlowResult:
+        """Manage the options."""
+        if user_input is not None:
+            return self.async_create_entry(title="", data=user_input)
+
+        return self.async_show_form(
+            step_id="init",
+            data_schema=vol.Schema(
+                {
+                    vol.Required(
+                        "pause_time",
+                        default=self.config_entry.options.get("pause_time", 5),
+                    ): int
+                }
+            ),
+        )
